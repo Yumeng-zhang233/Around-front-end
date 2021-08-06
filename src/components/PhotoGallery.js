c06cfce43f1868d8db4f6af6d3c42b891dc06d65
Yumeng-zhang233 <77811013+Yumeng-zhang233@users.noreply.github.com>
2021-08-05 20:28:28 -0700

    function add

---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react';
+import GridGallery from 'react-grid-gallery';
+
+function PhotoGallery(props) {
+    return (
+        <div>
+            Gallery
+        </div>
+    )
+}
+export default PhotoGallery;
+
+
